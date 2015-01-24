c968ec9ff4b220ba491e32dfde142b3d511399b2
Judicael Abecassis <judicael.abecassis@gmail.com>
2015-01-24 02:03:26 +0100

    Input mains clavier

--- Assets/Script/ControlTester.cs
+++ Assets/Script/ControlTester.cs
@@ -8,10 +8,20 @@ public class ControlTester : MonoBehaviour {
 	public float hFootLeft;
 	public float vFootLeft;
 
+	public float hHandRight;
+	public float vHandRight;
+	public float hHandLeft;
+	public float vHandLeft;
+
 	public string rightLegHorizontal = "Right Leg Horizontal Key";
 	public string rightLegVertical = "Right Leg Vertical Key";
 	public string leftLegHorizontal = "Left Leg Horizontal Key";
 	public string leftLegVertical = "Left Leg Vertical Key";
+
+	public string rightArmHorizontal = "Right Arm Horizontal Key";
+	public string rightArmVertical = "Right Arm Vertical Key";
+	public string leftArmHorizontal = "Left Arm Horizontal Key";
+	public string leftArmVertical = "Left Arm Vertical Key";
 
 	void Update()
 	{
@@ -19,5 +29,11 @@ public class ControlTester : MonoBehaviour {
 		  vFootRight = Input.GetAxis(rightLegVertical);
 		  hFootLeft = Input.GetAxis(leftLegHorizontal);
 		  vFootLeft = Input.GetAxis(leftLegVertical);
+
+		hHandRight = Input.GetAxis(rightArmHorizontal);
+		vHandRight = Input.GetAxis(rightArmVertical);
+		
+		hHandLeft = Input.GetAxis(leftArmHorizontal);
+		vHandLeft = Input.GetAxis(leftArmVertical);
 	}
 }
--- Assets/Script/PlayerControl.cs
+++ Assets/Script/PlayerControl.cs
@@ -10,6 +10,11 @@ public class PlayerControl : MonoBehaviour
 	public string leftLegHorizontal = "Left Leg Horizontal Key";
 	public string leftLegVertical = "Left Leg Vertical Key";
 
+	public string rightArmHorizontal = "Right Arm Horizontal Key";
+	public string rightArmVertical = "Right Arm Vertical Key";
+	public string leftArmHorizontal = "Left Arm Horizontal Key";
+	public string leftArmVertical = "Left Arm Vertical Key";
+
 	private GameObject footRight;
 	private GameObject footLeft;
 	private GameObject handRight;
@@ -27,11 +32,16 @@ public class PlayerControl : MonoBehaviour
 	{
 		float hFootRight = Input.GetAxis(rightLegHorizontal);
 		float vFootRight = Input.GetAxis(rightLegVertical);
-
 		float hFootLeft = Input.GetAxis(leftLegHorizontal);
 		float vFootLeft = Input.GetAxis(leftLegVertical);
 
+		float hHandRight = Input.GetAxis(rightArmHorizontal);
+		float vHandRight = Input.GetAxis(rightArmVertical);
+		float hHandLeft = Input.GetAxis(leftArmHorizontal);
+		float vHandLeft = Input.GetAxis(leftArmVertical);
+
 		MoveLegs(hFootRight, vFootRight, hFootLeft, vFootLeft);
+		MoveArms(hHandRight, vHandRight, hHandLeft, vHandLeft);
 	}
 
 	void MoveLegs(float hFootRight, float vFootRight, float hFootLeft, float vFootLeft)
@@ -50,5 +60,14 @@ public class PlayerControl : MonoBehaviour
 		Vector3 movement2 = new Vector3(hFootLeft,vFootLeft,0)*speed*Time.deltaTime;
 		footLeft.rigidbody2D.MovePosition(footLeft.transform.position + movement2);
 	}
-	
+
+	void MoveArms(float hHandtRight, float vHandRight, float hHandLeft, float vHandLeft)
+	{		
+		Vector3 movement = new Vector3(hHandtRight,vHandRight,0)*speed*Time.deltaTime;
+		handRight.rigidbody2D.MovePosition(handRight.transform.position + movement);
+		
+		Vector3 movement2 = new Vector3(hHandLeft,vHandLeft,0)*speed*Time.deltaTime;
+		handLeft.rigidbody2D.MovePosition(handLeft.transform.position + movement2);
+
+	}
 }
