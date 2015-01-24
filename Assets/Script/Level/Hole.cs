1af6d22241d6aba82e93cc42c07db79b9fffeb2c
Judicael Abecassis <judicael.abecassis@gmail.com>
2015-01-24 20:45:38 +0100

    Hole et plein d'autres trucs

---
+++ Assets/Script/Level/Hole.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hole : MonoBehaviour 
+{
+	public float strenght = 100f;
+
+	void OnTriggerStay2D(Collider2D other)
+	{
+		Vector3 direction = transform.position - other.gameObject.transform.position;
+		other.rigidbody2D.AddForce(direction * strenght * Time.deltaTime);
+	}
+
+}
