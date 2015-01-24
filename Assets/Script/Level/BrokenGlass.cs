dbc38e5f611dd7210888578d24aa13eaa6118d2c
Judicael Abecassis <judicael.abecassis@gmail.com>
2015-01-24 20:18:17 +0100

    Sprites To Mesh
    Remplacement des sprites par des mesh :(

---
+++ Assets/Script/Level/BrokenGlass.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BrokenGlass : MonoBehaviour 
+{
+	public float explosionForce = 10f;
+	public bool playOnAwake = true;
+
+	private Rigidbody2D[] pieces;
+
+	void Awake()
+	{
+		pieces = gameObject.GetComponentsInChildren<Rigidbody2D>();
+		if(playOnAwake)
+			Explode();
+	}
+
+	public void Explode()
+	{
+		Vector3 direction = Vector3.zero;
+		for(int i=0; i< pieces.Length; i++)
+		{
+			direction = Random.insideUnitCircle;
+			pieces[i].AddForce(direction.normalized * explosionForce);
+		}
+	}
+}
---
+++ Assets/Script/Level/GravityOnObject.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityOnObject : MonoBehaviour {
+	private float defaultGravity = 1;
+	private bool isGravityOn = true;
+	
+	public void SwitchGravity()
+	{
+		if(isGravityOn)
+			rigidbody2D.gravityScale = 0;
+		else
+			rigidbody2D.gravityScale = defaultGravity;
+		isGravityOn = !isGravityOn;
+	}
+	
+	public bool IsGravityOn
+	{
+		get
+		{
+			return isGravityOn;
+		}
+	}
+}
