4ad087d6c2ab94d790ae99a8fcd0131a9c334b81
nam evain <evain.jeremy@gmail.com>
2015-01-25 13:27:13 +0100

    level

--- Assets/Script/Level/GravityOnObject.cs
+++ Assets/Script/Level/GravityOnObject.cs
@@ -4,16 +4,21 @@ using System.Collections;
 public class GravityOnObject : MonoBehaviour {
 	private float defaultGravity = 1;
 	private bool isGravityOn = true;
-	
-	public void SwitchGravity()
-	{
-		if(isGravityOn)
-			rigidbody2D.gravityScale = 0;
-		else
-			rigidbody2D.gravityScale = defaultGravity;
-		isGravityOn = !isGravityOn;
-	}
-	
+
+    public void SwitchGravity()
+    {
+        if (isGravityOn)
+        {
+            rigidbody2D.gravityScale = 0;
+            rigidbody2D.AddForce(new Vector2(0, 100));
+        }
+
+        else
+        {
+            rigidbody2D.gravityScale = defaultGravity;
+            isGravityOn = !isGravityOn;
+        }
+    }
 	public bool IsGravityOn
 	{
 		get
