b5a9bee1a84b847a34a1163028be2e6a28727a22
nam evain <evain.jeremy@gmail.com>
2015-01-25 10:55:55 +0100

    level

---
+++ Assets/Script/Level/ObjectMouvement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectMouvement : MonoBehaviour {
+    Vector2 Impulsion;
+	// Use this for initialization
+	void Start () {
+       Impulsion = new Vector2 (Random.Range(-100F, 100F),Random.Range(-100F, 100f));
+       rigidbody2D.AddForce(Impulsion);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+   
+	}
+}
