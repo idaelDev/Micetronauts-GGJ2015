272bf8c57d284263bec6d5f86bac3d83f08853b8
Judicael Abecassis <judicael.abecassis@gmail.com>
2015-01-25 10:41:31 +0100

    nouveau script de camera

--- Assets/Script/Player/CameraFollow.cs
+++ Assets/Script/Player/CameraFollow.cs
@@ -5,8 +5,10 @@ public class CameraFollow : MonoBehaviour
 {
 	public Transform target;            // The position that that camera will be following.
 	public float smoothing = 5f;        // The speed with which the camera will be following.
-	
-	Vector3 offset;                     // The initial offset from the target.
+	public bool followY = false;
+	public float yToTarget;
+
+	Vector3 offset;                  // The initial offset from the target.
 	
 	void Start ()
 	{
@@ -17,9 +19,18 @@ public class CameraFollow : MonoBehaviour
 	void FixedUpdate ()
 	{
 		// Create a postion the camera is aiming for based on the offset from the target.
-		Vector3 targetCamPos = target.position + offset;
+		float x = target.position.x + offset.x;
+		float newPosX = Mathf.Lerp(transform.position.x, x, smoothing * Time.deltaTime);
+
+		float y = yToTarget;
+		float newPosY = 0;
+		if(followY)
+		{
+			y = target.position.y + offset.y;
+		}
+		newPosY = Mathf.Lerp(transform.position.y, y, smoothing * Time.deltaTime);
 		
 		// Smoothly interpolate between the camera's current position and it's target position.
-		transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing * Time.deltaTime);
+		transform.position =new Vector3(newPosX, newPosY, transform.position.z);
 	}
 }
