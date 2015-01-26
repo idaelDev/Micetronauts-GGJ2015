0ca3ec874d39e7e2b49a73cc291ef23f4f96298a
Judicael Abecassis <judicael.abecassis@gmail.com>
2015-01-26 13:32:06 +0100

    Correction level design

--- Assets/Script/Player/CameraFollow.cs
+++ Assets/Script/Player/CameraFollow.cs
@@ -7,19 +7,24 @@ public class CameraFollow : MonoBehaviour
 	public float smoothing = 5f;        // The speed with which the camera will be following.
 	public bool followY = false;
 	public float yToTarget;
+	public float xoffset = 0;
+	public float gravityZ = 8f;
+	public float noGravityZ = 5.5f;
 
+	public float zToTarget = 5.5f;
 	Vector3 offset;                  // The initial offset from the target.
-	
+	private Camera cam;
+
 	void Start ()
 	{
 		// Calculate the initial offset.
-		offset = transform.position - target.position;
+		cam = GetComponent<Camera>();
 	}
 	
 	void FixedUpdate ()
 	{
 		// Create a postion the camera is aiming for based on the offset from the target.
-		float x = target.position.x + offset.x;
+		float x = target.position.x + xoffset;
 		float newPosX = Mathf.Lerp(transform.position.x, x, smoothing * Time.deltaTime);
 
 		float y = yToTarget;
@@ -29,7 +34,11 @@ public class CameraFollow : MonoBehaviour
 			y = target.position.y + offset.y;
 		}
 		newPosY = Mathf.Lerp(transform.position.y, y, smoothing * Time.deltaTime);
-		
+
+		float z = zToTarget;
+		float newPosZ =  Mathf.Lerp(cam.orthographicSize, z, smoothing * Time.deltaTime);
+
+		cam.orthographicSize = newPosZ;
 		// Smoothly interpolate between the camera's current position and it's target position.
 		transform.position =new Vector3(newPosX, newPosY, transform.position.z);
 	}
